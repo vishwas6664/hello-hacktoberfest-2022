9c1912c5acbb921c33c43194353dbb805d7377de
Akshit Jain <82156880+akshit-jain@users.noreply.github.com>
2022-10-01 01:40:22 +0530

    Sorted Array to Binary Tree in leetcode solution

---
+++ arraytobinarytree.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return helper(nums, 0, nums.size()-1);
+    }
+    
+    TreeNode* helper(vector<int>& nums, int left, int right){
+        
+        //base case
+        //If the left pointer crosses right return null;
+        if(left > right){
+            return NULL;
+        }
